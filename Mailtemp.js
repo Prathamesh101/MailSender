f26e14ef242dc7aa9dc39616f985cb59dc8bb070
Prathamesh101 <tosheetk@gmail.com>
2020-05-28 15:54:33 +0530

    First Commit

---
+++ Mailtemp.js
@@ -0,0 +1,41 @@
+var nodemailer = require('nodemailer');
+var express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');  
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youruser@gmail.com',
+    pass: 'apna dal'
+  }
+});
+var app = express();
+app.use(express.static('./'));
+app.use(bodyParser());
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/home.html'));
+});
+
+app.post('/sendmail',function(req,res){
+  var mailOptions = {
+    from: 'toshk12345@gmail.com',
+    to: JSON.stringify(req.body.email),
+    subject: JSON.stringify(req.body.subject),
+    text: JSON.stringify(req.body.message)
+  
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.end("Mail Send successfully");
+});
+
+
+app.listen(3000, () => {
+  console.log('App running at http://localhost:3000');
+});
