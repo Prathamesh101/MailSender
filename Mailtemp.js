1d29cda9a76d448bb32b69673843252da55ff7af
Prathamesh Khandade <42217447+Prathamesh101@users.noreply.github.com>
2020-05-28 15:57:56 +0530

    Update Mailtemp.js

--- Mailtemp.js
+++ Mailtemp.js
@@ -6,7 +6,7 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'youruser@gmail.com',
-    pass: 'apna dal'
+    pass: 'Password'
   }
 });
 var app = express();
